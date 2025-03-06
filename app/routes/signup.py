d690dca79b34ef06660eefd84fadfa76d8a8c993
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-03-06 17:18:09 +0100

    del

--- app/routes/signup.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify, render_template
-
-bp = Blueprint('signup', __name__)
-
-@bp.route('/')
-def signup():
-    return render_template('signup.html', title="Auth", username="Hamid")
-    # return jsonify({'flights': 'list of auths'})
