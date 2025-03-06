b993ff09396bec851d4bd7fe0ea169fb790ff998
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-03-06 17:17:42 +0100

    feat

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,57 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
 import mysql.connector
-
+from datetime import timedelta
+from secret import jwt_key
+from flask_mail import Mail
 
 def create_app():
     app = Flask(__name__, static_folder="static")
     from config import Config
     app.config.from_object(Config)
-
-    db = mysql.connector.connect(
-        host=Config.DB_HOST,
-        user=Config.DB_USER,
-        password=Config.DB_PASSWORD,
-        database=Config.DB_NAME
-    )
+    app.config["JWT_SECRET_KEY"] = jwt_key
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     
-    # Import Blueprints only once and with unique names
+    app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+    app.config["JWT_ACCESS_COOKIE_NAME"] = "access_token_cookie"
+    app.config["JWT_COOKIE_SECURE"] = False
+    app.config["JWT_COOKIE_HTTPONLY"] = True
+    app.config["JWT_COOKIE_SAMESITE"] = "Lax"
+    app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+    jwt = JWTManager(app)
+    global mail
+    mail = Mail()
+    mail.init_app(app)
+    
+    # Blocklist storage
+    global revoked_tokens
+    revoked_tokens = set()
+    
+    @jwt.unauthorized_loader
+    def unauthorized_response(callback):
+        return jsonify({
+            "error": "You have to sign in", "redirect": "/auth/login"
+            }), 401
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_is_revoked(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in revoked_tokens
+
+    
+    # Import Blueprints
     from app.routes.flights import bp as flights_bp
     from app.routes.booking import bp as booking_bp
-    from app.routes.signup import bp as signup_bp
     from app.routes.flight_price import bp as flight_price_bp
-    from app.routes.login import bp as login_bp
+    from app.routes.auth import bp as auth_bp
+    from app.routes.logout import bp as logout_bp
     from app.routes.main import bp as main_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(flights_bp, url_prefix='/flights')
-    app.register_blueprint(booking_bp, url_prefix='/booking')
-    app.register_blueprint(signup_bp, url_prefix='/signup')
+    app.register_blueprint(booking_bp, url_prefix='/bookings')
     app.register_blueprint(flight_price_bp, url_prefix='/flight_price')
-    app.register_blueprint(login_bp, url_prefix='/login')
-    
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(logout_bp, url_prefix='/logout')
     
     return app
     
--- app/models/flight.py
+++ app/models/flight.py
@@ -10,3 +10,25 @@ class Flight(db.Model):
     price = db.Column(db.Integer, nullable=False)
     seats = db.Column(db.Integer, nullable=False)
     
+    
+    #     # if flight_result:
+    #     print("Flight found:", flight_result)
+    #     return render_template(
+    #         "flight_price.html",
+    #         departure_time=str(flight_result["departure_time"]),
+    #         arrival_time=str(flight_result["arrival_time"]),
+    #         price=price_result["price"] if price_result else default_price_result["price"]
+    #     )
+    # else:
+    #     print("No flight found, redirecting to index.")
+    #     return redirect(url_for("inde"))
+        # return render_template("flight_price.html", error="No flight found for this route")
+        
+        
+    # if flight_result:
+    #     return jsonify({
+    #         "departure_time": str(flight_result["departure_time"]),
+    #         "arrival_time": str(flight_result["arrival_time"]),
+    #         "price": price_result["price"] if price_result else default_price_result["price"]
+    #     })
+    
