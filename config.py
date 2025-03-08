a06c6796a39c9f7f3c03ea7e794f099273a7a8ad
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-03-08 07:48:21 +0100

    config update

--- config.py
+++ config.py
@@ -5,4 +5,33 @@ class Config:
     DB_PASSWORD = "NewPassword123"
     DB_NAME = "ht_booking"
     
-    # NewPassword123
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = "yhamid2828@gmail.com" 
+    MAIL_PASSWORD = "gylq eftx cspx gotg"
+    MAIL_DEFAULT_SENDER = "yhamid2828@gmail.com"
+    # gylq eftx cspx gotg
+    
+    
+    # test-user
+    # email - john@example.com
+    # pw - NewPassword123
+    
+    
+"""
+{
+    "first_name": "Idayat", 
+    "last_name": "Agbaje",
+    "email": "adeyinkah.28@gmail.com",
+    "confirm_email": "adeyinkah.28@gmail.com",
+    "address": "123 Street",
+    "city": "Instabul",
+    "mobile_number": "08059472483", 
+    "date_of_birth": "1990-01-07",
+    "gender": "Male",
+    "password": "Password123", 
+    "confirm_password": "Password123"
+}
+"""
