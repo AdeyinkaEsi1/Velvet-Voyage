5786130a3237fdc6da2ec18e01b4b4ad3b77cb54
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-03-09 08:18:54 +0100

    feat

--- app/models/flight.py
+++ app/models/flight.py
@@ -14,7 +14,7 @@ class Flight(db.Model):
     #     # if flight_result:
     #     print("Flight found:", flight_result)
     #     return render_template(
-    #         "flight_price.html",
+    #         "flight_price.html",-
     #         departure_time=str(flight_result["departure_time"]),
     #         arrival_time=str(flight_result["arrival_time"]),
     #         price=price_result["price"] if price_result else default_price_result["price"]
